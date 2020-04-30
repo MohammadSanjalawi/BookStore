fa48ac1144fcda14fd6f9c3c7e41f589944e7e68
Mohammad Sanjalawi <m7md-sanjalawi@hotmail.com>
2020-04-30 16:22:38 +0300

    add loyout and using bootstrap

--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -30,6 +30,7 @@ namespace BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
     }
